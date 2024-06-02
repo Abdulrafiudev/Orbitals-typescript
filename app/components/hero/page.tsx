9eafc369e37d141e28ff8f474817ade0ea8a1c12
Abdulrafiu-Python <abolajiibrahim70@gmail.com>
2024-06-02 14:57:28 +0100

    cmpleted hero page

--- app/components/hero/page.tsx
+++ app/components/hero/page.tsx
@@ -1,8 +1,33 @@
 import React from "react"
-import "./hero.module.css"
+import styles from "./hero.module.css"
+import hero_image from "../../../public/Frame 1000002719.png"
+import Image from "next/image"
 
 function Hero() {
-  return <div></div>
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero_container}>
+        <div className={styles.hero_container__first}>
+          <h1>
+            {" "}
+            AI voice Intelligence built to power popular low resource African
+            languages and more.
+          </h1>
+        </div>
+        <div className={styles.hero_container__second}>
+          <p>
+            {" "}
+            Fuelling Africa's future with advanced speech system that speaks the
+            language of progress and powering the next wave of African
+            innovation
+          </p>
+        </div>
+        <div className={styles.hero_container__third}>
+          <Image src={hero_image} alt="logo image" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
